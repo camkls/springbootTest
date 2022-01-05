6baf309da725d605e6dfa2c5febae8c0e8d53b9f
camklsOs <cam_kls@outlook.com>
2022-01-06 01:13:45 +0800

    add dog file to dev

---
+++ src/main/java/com/example/springboottest/testpack/Dog.java
@@ -0,0 +1,22 @@
+package com.example.springboottest.testpack;
+
+public class Dog {
+    private int dno;
+    private String dname;
+
+    public int getDno() {
+        return dno;
+    }
+
+    public void setDno(int dno) {
+        this.dno = dno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+}
