aeabe3bfd52256516a2bb537044dbf25f5d884c5
camklsOs <cam_kls@outlook.com>
2022-01-06 00:48:48 +0800

    add product on master

---
+++ src/main/java/com/example/springboottest/entity/product.java
@@ -0,0 +1,22 @@
+package com.example.springboottest.entity;
+
+public class product {
+    private int proNo;
+    private String proName;
+
+    public int getProNo() {
+        return proNo;
+    }
+
+    public void setProNo(int proNo) {
+        this.proNo = proNo;
+    }
+
+    public String getProName() {
+        return proName;
+    }
+
+    public void setProName(String proName) {
+        this.proName = proName;
+    }
+}
