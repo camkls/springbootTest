b12359f923d35526ab91571bcdd17ee12e222153
camkls <cam_kls@163.com>
2022-03-02 22:55:01 +0800

    11

--- src/main/java/com/example/springboottest/entity/User.java
+++ src/main/java/com/example/springboottest/entity/User.java
@@ -1,31 +1,13 @@
 package com.example.springboottest.entity;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(prefix = "x")
 public class User {
-    private int id;
-    private String name;
-    private String birthday;
+    private int xId;
+    private String xName;
+    private String xBirthday;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
 }
